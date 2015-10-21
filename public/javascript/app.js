d625f93876be3d0b80a89db160d1ded650cbbd7f
sinhvis <sinhvis@gmail.com>
2015-10-20 18:06:01 -0700

    Personal Website First commit

---
+++ public/javascript/app.js
@@ -0,0 +1,24 @@
+(function() {
+	'use strict';
+	angular.module('app', ['ui.router'])
+	.config(Config);
+	Config.$inject = ['$stateProvider', '$urlRouterProvider'];
+	function Config($stateProvider, $urlRouterProvider) {
+		$stateProvider.state('Home',{
+			url: '/',
+			templateUrl: 'templates/home.html',
+			controller: 'HomeController',
+			controllerAs: 'vm'
+		}).state('Projects',{
+			url: '/projects',
+			templateUrl: 'templates/project.html',
+		}).state('Resume',{
+			url: '/resume',
+			templateUrl: 'templates/resume.html',
+		}).state('Contact',{
+			url: '/contact',
+			templateUrl: 'templates/contact.html',
+		})
+		$urlRouterProvider.otherwise('/');
+	}
+})();
